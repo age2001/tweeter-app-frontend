6a899a8fa35de02dac385a77f412881e2f23c7ee
age2001 <alan-enriquez@live.com>
2024-04-11 09:58:08 -0500

    Removed user dependency in post and reply models

--- tweeter-app-frontend/src/app/models/post.model.ts
+++ tweeter-app-frontend/src/app/models/post.model.ts
@@ -1,7 +1,4 @@
-import { IUser } from "./user.model";
-
 export interface IPost {
     id: number;
     content: string;
-    user: IUser;
 }
--- tweeter-app-frontend/src/app/models/reply.model.ts
+++ tweeter-app-frontend/src/app/models/reply.model.ts
@@ -1,7 +1,5 @@
-import { IUser } from './user.model';
 
 export interface IReply {
     id: number;
     content: string;
-    user: IUser;
 }
