8ec657c9dd5f8739a6cf5fa94afb5e59c4c6eb53
age2001 <alan-enriquez@live.com>
2024-04-11 09:28:06 -0500

    Removed angular boilerplate page. Created post, reply, tag, and user models. Created home, login, profile, and register pages.

--- tweeter-app-frontend/src/app/app.module.ts
+++ tweeter-app-frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    ProfilePageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
