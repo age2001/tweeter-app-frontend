eb283362a6f01e26792e3702f94c020fe2c39896
age2001 <alan-enriquez@live.com>
2024-04-11 11:21:36 -0500

    Implemented login and register page html forms

--- tweeter-app-frontend/src/app/pages/register-page/register-page.component.ts
+++ tweeter-app-frontend/src/app/pages/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IUser } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  
+  userModel: IUser = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    userName: '',
+    password: '',
+    phoneNumber: ''
+  };
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmitHandler() {
+    this.authService.register(this.userModel).subscribe((response: any) => {
+      console.log(response);
+      this.router.navigate(['/login'], { queryParams: { registered: 'true' } });
+    }, (error: any) => {
+      console.log(error);
+    });
+  }
 }
