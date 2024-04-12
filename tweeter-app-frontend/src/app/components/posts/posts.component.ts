750ce97d5ba6e7ac5b56acf61ccf6401258f4595
age2001 <alan-enriquez@live.com>
2024-04-12 09:47:08 -0500

    Fixed bug where posts component wasn't entering conditional statements to get tweets

--- tweeter-app-frontend/src/app/components/posts/posts.component.ts
+++ tweeter-app-frontend/src/app/components/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IPostInfo } from 'src/app/models/post-info.model';
 import { DataService } from 'src/app/services/data.service';
@@ -9,23 +9,25 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
-  
-  @Input()
   pageName: any;
   username: any;
   posts: IPostInfo[] = [];
 
-  constructor(private dataService: DataService, private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private dataService: DataService, private activatedRoute: ActivatedRoute, private router: Router, private elementRef: ElementRef) {
     this.username = this.activatedRoute.snapshot.paramMap.get('username');
+    this.pageName = this.elementRef.nativeElement.getAttribute('pageName');
     // If on Home page posts feed then get all posts
     // Else if on the users profile page, get posts by the users stored username
     if (this.pageName === 'allPosts') {
       this.dataService.getPosts().subscribe((response: any) => {
+        console.log("all posts detected")
         console.log(response);
         this.posts = response;
+        // this.posts = 
       });
     } else if (this.pageName === 'profilePosts') {
       this.dataService.getPostsByUsername(this.username).subscribe((response: any) => {
+        console.log("profile posts detected")
         console.log(response);
         this.posts = response;
       });
